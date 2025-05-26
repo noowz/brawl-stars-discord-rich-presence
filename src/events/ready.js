d93c84dbe762f7c36cd46846dfe519b8a6b7a07e
Noowz <danieeel.dev@gmail.com>
2025-05-26 17:10:13 +0200

    🔨 Updated logger

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,11 @@
 const { logger } = require("../utils/logger.js");
 const { rpc } = require("../rpc/rpc.js");
+const { green } = require("chalk");
 const { Events } = require("discord.js");
 
 const ready = {
-    name: Events.ClientReady,
-    once: true,
+	name: Events.ClientReady,
+	once: true,
 
 	execute(client) {
 		rpc(client);
@@ -13,7 +14,7 @@ const ready = {
 			rpc(client);
 		}, 10000);
 
-		logger.info("Rich Presence connected to Discord!");
+		logger.info(`Rich Presence ${green("connected")} to Discord!`);
 	}
 };
 
