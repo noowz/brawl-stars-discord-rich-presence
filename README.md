a2a3f30b34b477b0eba08b2d3b85b2ae5a5c99e4
Noowz <danieeel.dev@gmail.com>
2025-11-04 01:37:55 +0100

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,81 @@
+![brawl-stars-discord-rich-presence](https://socialify.git.ci/noowz/brawl-stars-discord-rich-presence/image?description=1&forks=1&issues=1&language=1&name=1&owner=1&pattern=Solid&pulls=1&stargazers=1&theme=Auto)
+
+---
+
+<p align="center">
+	<a href="#-about">About</a> |
+  	<a href="#%EF%B8%8F-preview">Preview</a> |
+  	<a href="#%EF%B8%8F-installation">Installation</a> |
+  	<a href="#-how-to-use">How to use</a> |
+	<a href="#-contribute">Contribute</a> |
+	<a href="#-license">License</a> |
+	<a href="#-disclaimer">Disclaimer</a> |
+</p>
+
+---
+
+## **📙 About**
+
+**brawl-stars-discord-rich-presence** is a Discord Rich Presence that shows on your profile your Brawl Stars statistics.
+
+---
+
+## **🖼️ Preview**
+
+![Discord Rich Presence](https://i.imgur.com/F7uSi9y.png)
+
+---
+
+## **⚒️ Installation**
+
+1. Download [Node.js](https://nodejs.org/en/download).
+2. Download [brawl-stars-discord-rich-presence](https://github.com/noowz/brawl-stars-discord-rich-presence/releases/latest).
+
+---
+
+## **🚀 How to use**
+
+1. Open **Brawl Stars** and follow the steps to get your player tag:
+
+   ![Brawl Stars Player Tag](https://i.imgur.com/F5OFwv8.gif)
+
+2. Rename the `.env.example` file into `.env`.
+3. Go to the `.env` file and replace **BRAWL_STARS_PLAYER_TAG** with your Brawl Stars player tag.
+4. Go to the [Brawl Stars API Dashboard](https://developer.brawlstars.com) and create an account or log in to your account.
+5. Create a [new API key](https://developer.brawlstars.com/#/new-key). You will need to allow your IP Address. To find out your IP Address, [click here](https://nordvpn.com/what-is-my-ip).
+6. Go to the `.env` file and replace **BRAWL_STARS_API_KEY** with your API key.
+7. Open the Terminal and go to the folder where you saved the files with the `cd` command.
+8. Install all the required dependencies:
+
+   ```cmd
+   npm install
+   ```
+
+9. Run the Rich Presence:
+
+   ```cmd
+   npm run start
+   ```
+
+> [!NOTE]
+> For the Rich Presence to work, you need to have Discord open in your computer. Also, make sure to turn on your Activity Status by going to **User Settings > Activity Privacy > Share my activity**.
+
+---
+
+## 🐛 **Contribute**
+
+If you found a bug or want to request a feature, browse the open [issues](https://github.com/noowz/brawl-stars-discord-rich-presence/issues) before opening a new one.
+
+If you know how to fix an [issue](https://github.com/noowz/brawl-stars-discord-rich-presence/issues), consider opening a [pull request](https://github.com/noowz/brawl-stars-discord-rich-presence/pulls) for it.
+
+---
+
+## 📝 **License**
+
+This project is licensed under the [MIT](./LICENSE) license.
+
+---
+
+## 📌 **Disclaimer**
+
+> This material is unofficial and is not endorsed by Supercell. For more information see Supercell's Fan Content Policy: [Supercell’s Fan Content Policy](https://supercell.com/fan-content-policy).
---
+++ src/rpc/rpc.js
@@ -0,0 +1,118 @@
+const { name, version, repository } = require("../../package.json");
+const { apis } = require("../config.json");
+const { logger } = require("../utils/logger");
+const axios = require("axios");
+const { app } = require("google-play-scraper");
+
+let firstTimeRunningRichPresence = true;
+
+let startDate = firstTimeRunningRichPresence
+	? Date.now()
+	: startDate;
+
+const rpc = async function setActivity(client) {
+	let brawlAPINutellaResponseNull = false;
+
+	const brawlstarsResponse = await axios({
+		method: "GET",
+		url: `${apis.brawlstars.base_url}/v1/players/%23${process.env.BRAWL_STARS_PLAYER_TAG.replace("#", "")}`,
+		headers: {
+			Authorization: `Bearer ${process.env.BRAWL_STARS_API_KEY}`,
+			"Content-Type": "application/json",
+			"User-Agent": `${name}/${version}`
+		}
+	}).catch((error) => {
+		logger.error(error);
+
+		process.exit(1);
+	});
+
+	const brawlAPINutellaResponse = await axios({
+		method: "GET",
+		url: `${apis.brawlAPINutella.base_url}/profile`,
+		params: {
+			tag: process.env.BRAWL_STARS_PLAYER_TAG.replace("#", "")
+		},
+		headers: {
+			"Content-Type": "application/json",
+			"User-Agent": `${name}/${version}`
+		},
+	}).catch((error) => {
+		logger.error(error);
+
+		process.exit(1);
+	});
+
+	if (!brawlAPINutellaResponseNull && brawlAPINutellaResponse.data.result === null) {
+		brawlAPINutellaResponseNull = true;
+
+		logger.warn("BrawlAPINutella returned an empty response. Ignoring BrawlAPINutella data.");
+	};
+
+	const player = brawlAPINutellaResponseNull
+		? {
+			...brawlstarsResponse.data
+		}
+		: {
+			...brawlstarsResponse.data,
+			...brawlAPINutellaResponse.data.result
+		};
+
+	const rankedRanks = [
+		"Bronze I",
+		"Bronze II",
+		"Bronze III",
+		"Silver I",
+		"Silver II",
+		"Silver III",
+		"Gold I",
+		"Gold II",
+		"Gold III",
+		"Diamond I",
+		"Diamond II",
+		"Diamond III",
+		"Mythic I",
+		"Mythic II",
+		"Mythic III",
+		"Legendary I",
+		"Legendary II",
+		"Legendary III",
+		"Masters I",
+		"Masters II",
+		"Masters III",
+		"Pro"
+	];
+
+	const gameApp = await app({
+		appId: "com.supercell.brawlstars"
+	});
+
+	client.request("SET_ACTIVITY", {
+		pid: process.pid,
+		activity: {
+			details: brawlAPINutellaResponseNull
+				? `🏆 Trophies: ${player.trophies}/${player.highestTrophies}`
+				: `🏆 Trophies: ${player.trophies}/${player.highestTrophies} • 🏅 Rank: ${rankedRanks[player.stats.find(stat => stat.id === 23).value - 1]}/${player.stats.find(stat => stat.id === 22).value === 0 ? rankedRanks[player.stats.find(stat => stat.id === 22).value] : rankedRanks[player.stats.find(stat => stat.id === 22).value - 1]} (${player.stats.find(stat => stat.id === 24).value}/${player.stats.find(stat => stat.id === 25).value})`,
+			state: brawlAPINutellaResponseNull ? `🥊 3 vs 3 Victories: ${player["3vs3Victories"]} • 💀 Victories: ${player.soloVictories + player.duoVictories}` : `🥊 3 vs 3 Victories: ${player["3vs3Victories"]} • 💀 Victories: ${player.soloVictories + player.duoVictories} • 🔥 Max Win Streak: ${player.max_winstreak} • 👁️ Record Points: ${player.stats.find(stat => stat.id === 31).value} (Level ${player.stats.find(stat => stat.id === 32).value})`,
+			timestamps: {
+				start: startDate
+			},
+			assets: {
+				large_image: gameApp.icon,
+				large_text: gameApp.title,
+				small_image: `https://cdn.brawlify.com/profile-icons/regular/${player.icon.id}.png`,
+				small_text: `${player.name} (${player.tag})`
+			},
+			buttons: [
+				{
+					label: "🚀 Download",
+					url: repository.url
+				}
+			]
+		}
+	});
+};
+
+firstTimeRunningRichPresence = false;
+
+module.exports = { rpc };
